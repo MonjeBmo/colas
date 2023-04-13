13c28c7876ab41102b03a1b476a4858a48684e8f
Victor Monje <monjev086@gmail.com>
2023-04-12 21:38:11 -0600

    add op { 1, 2 }

--- labo#7/main.cpp
+++ labo#7/main.cpp
@@ -4,10 +4,123 @@
 
 using namespace std;
 
+struct nodoCola
+{
+    char dato;
+    struct nodoCola *ptr_S;
+};
+
+typedef struct nodoCola NodoCola;
+typedef NodoCola *ptrNodoCola;
+
+void printCola(ptrNodoCola ptrA);
+void add(ptrNodoCola *ptrHead, ptrNodoCola *ptrTalon, char value);
+void instruction(void);
+
 // Laboratorio #7
 int main()
 {
+    ptrNodoCola ptrHead = NULL;
+    ptrNodoCola ptrTalon = NULL;
+    int op;
+    char element;
+    instruction();
+    cout << "Que desea realizar? ";
+    cin >> op;
+
+    while (op != 6)
+    {
+        system("clear");
+        switch (op)
+        {
+        case 1:
+            cout << "Introduzca un caracter : ";
+            cin >> element;
+            add(&ptrHead, &ptrTalon, element);
+            printCola(ptrHead);
+            break;
+        case 2:
+            printCola(ptrHead);
+            break;
+
+        default:
+            cout << "Eleccion no Valida";
+            break;
+        }
+        instruction();
+        cout << "Que desea realizar? \n\n";
+
+        cin >> op;
+    }
+
+    cout << "fin app\n\n";
+
     return 0;
 }
 
+void instruction(void)
+{
+    cout << "Introduzca su eleccion \n";
+    cout << "1.- Ingresar a la cola\n";
+    cout << "2.- Mostrar Cola\n";
+    cout << "3.- Buscar Elemento en la cola\n";
+    cout << "4.- Eliminar 1re. Elemento\n";
+    cout << "5.- Elimniar # de Elementos\n";
+    cout << "6.- Salir\n";
+}
 
+int isEmpty(ptrNodoCola ptrHead)
+{
+    return ptrHead == NULL;
+}
+
+void add(ptrNodoCola *ptrHead, ptrNodoCola *ptrTalon, char value)
+{
+    if (isdigit(value))
+    {
+        ptrNodoCola ptrNew;
+        ptrNew = (NodoCola *)malloc(sizeof(NodoCola));
+        if (ptrNew != NULL)
+        {
+            ptrNew->dato = value;
+            ptrNew->ptr_S = NULL;
+
+            if (isEmpty(*ptrHead))
+            {
+                *ptrHead = ptrNew;
+            }
+            else
+            {
+                (*ptrTalon)->ptr_S = ptrNew;
+            }
+
+            *ptrTalon = ptrNew;
+        }
+        else
+        {
+            cout << "no se incerto " << value << "No hay memoria disponible";
+        }
+    }
+
+    cout << "\nNO es un digito!\n";
+}
+
+void printCola(ptrNodoCola ptrA)
+{
+    if (ptrA == NULL)
+    {
+        cout << "la cola esta vacia!" << endl
+             << endl;
+    }
+    else
+    {
+        cout << "la cola es : \n";
+        while (ptrA != NULL)
+        {
+            cout << ptrA->dato << " -> ";
+            ptrA = ptrA->ptr_S;
+        }
+
+        cout << "NULL\n\n";
+    }
+}
